d27a64be7b8ccde7b2bea5d894c4239d373f4bd1
CrisUltraHD <63653399+CrisUltraHD@users.noreply.github.com>
2021-03-17 12:27:51 +0100

    Add files via upload

---
+++ motauto/org.eclipse.papyrus.javagen.motauto/src/motauto/AlterarEstructuraBBDD.java
@@ -0,0 +1,184 @@
+package motauto;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import com.ibatis.common.jdbc.ScriptRunner;
+
+public class AlterarEstructuraBBDD {
+
+	public static Database createBDD() {
+		Database db = null;
+		try {
+			if (Utiles.siNo("Seguro que quieres crear una nueva BDD?")) {
+				db = AlterarEstructuraBBDD.configuracionBDD();
+				Connection conex = db.conectToBDD();
+				System.out.println(db.ExecuteCreate("CREATE DATABASE " + db.GetDB()) ? "Correcte" : "Incorrecte");
+				conex = db.conectToBDD(db.GetDB());
+				ScriptRunner runner = new ScriptRunner(conex, false, false);
+				InputStreamReader reader;
+
+				Scanner scanner;
+				String ruta = "Scripts\\datosConex.txt";
+				File carpeta = new File(ruta);
+				scanner = new Scanner(carpeta);
+				String linea = scanner.nextLine();
+				String[] datos = linea.split("#");
+				scanner.close();
+				db = new Database(datos[0], datos[1], datos[2], datos[3], datos[4]);
+				System.out.println(db.connectDatabase() ? "Prueba de Conexion positiva." : "Error en la Conexion");
+
+				reader = new InputStreamReader(new FileInputStream("Scripts\\CreacionBaseDatos.sql"), "UTF8");
+
+				runner.runScript(reader);
+				reader.close();
+			}
+
+		} catch (Exception e) {
+			System.out.println("error: " + e);
+		}
+		return db;
+	}
+
+	public static boolean updateBDD(Database db) {
+		boolean correct;
+		try {
+			String ruta = "Scripts\\";
+			File carpeta = new File(ruta);
+			String[] nomDelsFichers = carpeta.list();
+			Connection conex = db.GetConnection();
+			ScriptRunner runner = new ScriptRunner(conex, false, false);
+			InputStreamReader reader;
+			System.out.println("Posa el nom del Script a Carregar: ");
+			for (int i = 0; i < nomDelsFichers.length; i++) {
+				System.out.println("- " + nomDelsFichers[i]);
+			}
+			reader = new InputStreamReader(new FileInputStream("Scripts\\" + Utiles.demanarString() + ".sql"), "UTF8");
+			runner.runScript(reader);
+			reader.close();
+			correct = true;
+		} catch (Exception e) {
+			correct = false;
+		}
+		return correct;
+	}
+
+	public static Database configuracionBDD() {
+		String ruta = "Scripts\\datosConex.txt";
+		File archivo = new File(ruta);
+		int opcio;
+		BufferedWriter bw;
+		String driver = "org.mariadb.jdbc.Driver";
+		System.out.println("Diges la base de dades on treballaras: ");
+		String db = Utiles.demanarString();
+		System.out.println("Diges l'usuari de la base de dades: ");
+		String user = Utiles.demanarString();
+		System.out.println("Diges la contrasenya de la base de dades: ");
+		String passwd = Utiles.demanarString();
+		System.out.println("Diges la IP de la base de dades: (nomes la Ip)");
+		String ip = Utiles.demanarString();
+		String url = "jdbc:mysql://" + ip + "/";
+		System.out.println("Quin es la base de dades utilitzada? ");
+		System.out.println("0- MariaDB ");
+		System.out.println("1- PostGress ");
+		System.out.println("2- MySQL ");
+
+		opcio = Utiles.menu(3);
+		switch (opcio) {
+		case 0:
+			driver = "org.mariadb.jdbc.Driver";
+			break;
+		case 1:
+			driver = "org.postgresql.Driver";
+			break;
+		case 2:
+			driver = "com.mysql.jdbc.Driver";
+			break;
+
+		}
+		try {
+			bw = new BufferedWriter(new FileWriter(archivo));
+			bw.write(db + "#" + user + "#" + passwd + "#" + url + "#" + driver);
+			bw.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Database conexion = new Database(db, user, passwd, url, driver);
+		System.out.println(conexion.connectDatabase() ? "Conexio Correcte" : "Conexio Incorrecte");
+		return conexion;
+
+	}
+
+	public static void showTables(Database db) {
+		ArrayList<String> tables = new ArrayList<String>();
+
+		try {
+			ResultSet rs = db.ExecuteQuery("SHOW TABLES");
+			while (rs.next()) {
+				tables.add(rs.getString(1));
+			}
+			for (int i = 0; i < tables.size(); i++) {
+				rs = db.ExecuteQuery("SHOW COLUMNS FROM " + tables.get(i));
+				System.out.println("- " + tables.get(i));
+				while (rs.next()) {
+					System.out.print(rs.getString(1) + " / ");
+				}
+				System.out.println("\n");
+			}
+
+		} catch (Exception e) {
+
+		}
+	}
+
+	public static void sqlInyector(Database db) {
+		ArrayList<String> commands = new ArrayList<String>();
+		String text = "";
+		System.out.println("Escribe las comandas para inyectar en la BDD seguidas de ';' i enter.\nexecute para ejecutarlas");
+		while (!text.equalsIgnoreCase("Execute")) {
+			text = Utiles.demanarString();
+			if (!text.equalsIgnoreCase("Execute"))
+				commands.add(text);
+		}
+		for (int i = 0; i < commands.size(); i++) {
+			try {
+				System.out.println(db.ExecuteUpdate(commands.get(i)) ? "Correcte" : "Correcte");
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static Database establecerPrimeraConexion() throws FileNotFoundException {
+		String ruta = "Scripts\\datosConex.txt";
+		File archivo = new File(ruta);
+		Database db = null;
+		if (archivo.exists()) {
+			Scanner scanner;
+			scanner = new Scanner(archivo);
+			String linea = scanner.nextLine();
+			String[] datos = linea.split("#");
+			scanner.close();
+			db = new Database(datos[0], datos[1], datos[2], datos[3], datos[4]);
+			System.out.println(db.connectDatabase() ? "Prueba de Conexion positiva." : "Error en la Conexion");
+		} else {
+			System.out.println("No hemos detectado un archivo de conexion existente, vamos a crearlo.");
+			db = AlterarEstructuraBBDD.configuracionBDD();
+			System.out.println(db.connectDatabase() ? "Prueba de Conexion positiva." : "Error en la Conexion");
+		}
+		return db;
+	}
+
+}
---
+++ motauto/org.eclipse.papyrus.javagen.motauto/src/motauto/Database.java
@@ -0,0 +1,184 @@
+package motauto;
+import java.sql.*;
+
+public class Database {
+	
+    // Atributs per a la connexió
+    private String db;
+    private String user;
+    private String passwd;
+    private String url ;
+    private String driver;
+    // Connexió
+    private Connection connection;
+    private Statement st;
+
+    // Constructor
+    public Database(String db, String user, String passwd, String url, String driver) {
+	this.db= db;
+	this.user=user;
+	this.passwd=passwd;
+	this.url=url;
+	this.driver=driver;
+        
+    }
+    public Database(String user, String passwd, String url, String driver) {
+    	this.user=user;
+    	this.passwd=passwd;
+    	this.url=url;
+    	this.driver=driver;
+    }
+
+    // Setters
+    public void SetDB(String sDB) {
+        db = sDB;
+        url += sDB;
+    }
+
+    public void SetUser(String sUser) {
+        user = sUser;
+    }
+
+    public void SetPasswd(String sPasswd) {
+        passwd = sPasswd;
+    }
+
+    public void SetURL(String sURL) {
+        url = sURL;
+    }
+
+    // Getters
+    public String GetDB() {
+        return db;
+    }
+
+    public String GetUser() {
+        return user;
+    }
+
+    public String GetPasswd() {
+        return passwd;
+    }
+
+    public String GetURL() {
+        return url;
+    }
+
+    public Connection GetConnection() {
+        return connection;
+    }
+
+    // Metodes
+    public boolean connectDatabase() {
+    	boolean conexValida=false;
+        try {
+            // Registro el driver de PostgresSQL
+            try {
+                Class.forName(driver);
+            } catch (ClassNotFoundException ex) {
+                System.out.println("Error al registrar el driver de MySQL: " + ex);
+            }
+            // Conecta amb la base de dades
+            connection = DriverManager.getConnection(url+db, user, passwd);
+
+            boolean valid = connection.isValid(50000);
+            conexValida= (valid ? true : false);
+        } catch (SQLException sqle) {
+            System.out.println("Error: " + sqle);
+        }
+        return conexValida;
+    }
+    
+
+    // Metode per executar sentencies SQL que retornen resultats (basicament
+    // SELECTS)
+    public ResultSet ExecuteQuery(String query) throws SQLException {
+        connectDatabase();
+        st = connection.createStatement();
+
+        
+        return st.executeQuery(query);
+
+    }
+
+    // Metode per executar sentencies SQL que no retornen resultats (INSERTS,
+    // UPDATES, DELETES)
+    public boolean ExecuteUpdate(String query) throws SQLException {
+		boolean ejecucionCorrecta;
+        connectDatabase();
+
+        st = connection.createStatement();
+        int correcte = st.executeUpdate(query);
+		if (correcte ==1){
+			ejecucionCorrecta=true;
+		}
+		else{
+			ejecucionCorrecta=false;
+		}
+		
+
+		return ejecucionCorrecta;
+    }
+    public boolean ExecuteCreate(String query) throws SQLException {
+		boolean ejecucionCorrecta;
+		connection = conectToBDD(this.db);
+
+        st = connection.createStatement();
+        int correcte = st.executeUpdate(query);
+		if (correcte ==1){
+			ejecucionCorrecta=true;
+		}
+		else{
+			ejecucionCorrecta=false;
+		}
+				
+		
+		return ejecucionCorrecta;
+    }
+    public Connection conectToBDD() {
+        try {
+
+            try {
+                Class.forName(driver);
+            } catch (ClassNotFoundException ex) {
+                System.out.println("Error al registrar el driver de MySQL: " + ex);
+            }
+
+            connection = DriverManager.getConnection(url, user, passwd);
+
+            boolean valid = connection.isValid(50000);
+            System.out.println(valid ? "Conex On" : "Conex off");
+            
+        } catch (SQLException sqle) {
+            System.out.println("Error: " + sqle);
+        }
+        
+        return connection;
+    }
+    public Connection conectToBDD(String db) {
+    	boolean conexValida=false;
+        try {
+           
+            try {
+            	Class.forName(driver);
+            } catch (ClassNotFoundException ex) {
+                System.out.println("Error al registrar el driver de MySQL: " + ex);
+            }
+
+            connection = DriverManager.getConnection(url+db, user, passwd);
+
+            boolean valid = connection.isValid(50000);
+            conexValida= (valid ? true : false);
+        } catch (SQLException sqle) {
+            System.out.println("Error: " + sqle);
+        }
+        return connection;
+    }
+    public void showAll() {
+    	System.out.println("Base de dades seleccionada: " + this.db);
+    	System.out.println("Usuari: " + this.user);
+    	System.out.println("Contraseña: " + this.passwd);
+    	System.out.println("Ip Servidor: " + this.url);
+    	
+    }
+}
