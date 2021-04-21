5a2f3a52280a0c353b8dadd14aff8e5bd455dd55
CrisUltraHD <63653399+CrisUltraHD@users.noreply.github.com>
2021-04-21 17:57:41 +0200

    Add files via upload

---
+++ Scripts/BateriaDatos.sql
@@ -0,0 +1,21 @@
+INSERT INTO articulos (codigo, nombre, precio, iva) VALUES ("M001","Mano de obra", 30, 0.21);
+INSERT INTO articulos (codigo, nombre, precio, iva) VALUES ("A001","Filtro aceite Bosch", 15, 0.21);
+INSERT INTO articulos (codigo, nombre, precio, iva) VALUES ("A002","Filtro aire K&n", 70.29, 0.21);
+INSERT INTO articulos (codigo, nombre, precio, iva) VALUES ("A003","Aceite Motul 4100 Performance 10W-40, 5L", 23.97, 0.21);
+INSERT INTO articulos (codigo, nombre, precio, iva) VALUES ("A004","Escobilla Bosch Aerotwin A8635 650mm/450mm", 19.68, 0.21);
+INSERT INTO articulos (codigo, nombre, precio, iva) VALUES ("S001","Calibració rodes", 45, 0.21);
+
+
+INSERT INTO clientes (dni, nombre, apellidos , correo, telefono, direccion) VALUES ("41528630Z","Pedro","Ponce Sanchez", "pereponce7@gmail.com", 622072104, "pozo alcon 62");
+INSERT INTO clientes (dni, nombre, apellidos , correo, telefono, direccion) VALUES ("41508030A","Jordi","Poch", "info@instalaciónspoch.com", 663728231, "Instal·lacions Poch");
+INSERT INTO clientes (dni, nombre, apellidos , correo, telefono, direccion) VALUES ("42222222f","Cristian","Caride Rodriz", "cariderodriguez@gmail.com", 662471231, "Napoles 17255");
+INSERT INTO clientes (dni, nombre, apellidos , correo, telefono, direccion) VALUES ("42285782Q","Derar","El Koumssi", "delkoumssi@gmail.com", 622092104, "Villademany 5");
+INSERT INTO clientes (dni, nombre, apellidos , correo, telefono, direccion) VALUES ("41509630Z","Sergio","Deulofeu Blinder", "deulowtf@gmail.com", 611072104, "Birmingham 69");
+
+
+
+INSERT INTO vehiculo (matricula, dni, color , tipo) VALUES ("4040MKR", "41528630Z", "gris", "coche");
+INSERT INTO vehiculo (matricula, dni, color , tipo) VALUES ("666DKR", "42222222f", "blau", "furgoneta");
+INSERT INTO vehiculo (matricula, dni, color , tipo) VALUES ("777AAA", "41508030A", "groc", "camieno");
+INSERT INTO vehiculo (matricula, dni, color , tipo) VALUES ("888BBB", "42285782Q", "verd", "moto");
+INSERT INTO vehiculo (matricula, dni, color , tipo) VALUES ("999CCC", "41509630Z", "vermell", "coche");
